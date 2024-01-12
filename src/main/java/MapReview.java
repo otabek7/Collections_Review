b6ac411d3bb6e53c611c114a11df613c7db06eac
Otabek <mavlonovo@tamu.edu>
2024-01-11 21:31:22 -0600

    added hash map

---
+++ src/main/java/MapReview.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Sergio");
+        map.put(2, "Moses");
+        System.out.println(map.get(2));
+
+        System.out.println(findFirstNonRepeating("you are repeating yourself"));
+    }
+
+    public static Character findFirstNonRepeating(String str){
+        Map<Character, Integer> map = new HashMap<>();
+        //For counting char occurrences
+        int count;
+
+        for(Character ch:str.toCharArray()){
+            if(map.containsKey(ch)){
+                count = map.get(ch);
+                map.put(ch, count+1);
+            }else{
+                map.put(ch, 1);
+            }
+        }
+        for (Character ch:str.toCharArray()){
+            if(map.get(ch)==1)
+                return ch;
+        }
+        return Character.MIN_VALUE;
+
+    }
+}
